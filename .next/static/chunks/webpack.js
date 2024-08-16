14a901f714792e3718a9bc8cb2bf796d5a5b6ba3
ALexander Cosma <acosma04@gmail.com>
2024-08-15 21:18:22 -0400

    2nd Commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b9a6178d99496a86"; }
+/******/ 		__webpack_require__.h = function() { return "c905077bec93ffc6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
